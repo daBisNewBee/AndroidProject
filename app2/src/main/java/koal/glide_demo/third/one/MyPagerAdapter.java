4df24663dbe6371d28f37697cad51f48c7688816
wenbin.liu <wenbin.liu@ximalaya.com>
2019-06-19 20:37:22 +0800

    新增ViewPager用法

---
+++ app2/src/main/java/koal/glide_demo/third/one/MyPagerAdapter.java
@@ -0,0 +1,50 @@
+package koal.glide_demo.third.one;
+
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by wenbin.liu on 2019-06-17
+ *
+ * @author wenbin.liu
+ */
+public class MyPagerAdapter extends PagerAdapter {
+
+    private ArrayList<View> mViewsList;
+
+    public MyPagerAdapter() {
+    }
+
+    public MyPagerAdapter(ArrayList<View> viewsList) {
+        super();
+        this.mViewsList = viewsList;
+    }
+
+    @Override
+    public int getCount() {
+        return mViewsList.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Log.v("todo", "instantiateItem container = [" + container + "], position = [" + position + "]");
+        View view = mViewsList.get(position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        Log.v("todo", "destroyItem container = [" + container + "], position = [" + position + "], object = [" + object + "]");
+        container.removeView(mViewsList.get(position));
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
