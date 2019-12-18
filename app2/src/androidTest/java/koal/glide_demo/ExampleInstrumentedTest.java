0597ae43f9e0d00d0e453bfc0829923f04be3e96
liuwenbin <liuwb@koal.com>
2019-12-19 01:26:26 +0800

    MediaPlayer 对srt字幕同步的解析。TODO：终端打印中文乱码

--- app2/src/androidTest/java/koal/glide_demo/ExampleInstrumentedTest.java
+++ app2/src/androidTest/java/koal/glide_demo/ExampleInstrumentedTest.java
@@ -1,11 +1,20 @@
 package koal.glide_demo;
 
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.TimedText;
+import android.os.Looper;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.*;
 
@@ -23,5 +32,63 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("koal.glide_demo", appContext.getPackageName());
+    }
+
+    @Test
+    public void MPTest() throws Exception {
+        MediaPlayer player = new MediaPlayer();
+        assertTrue(player != null);
+
+        player.setDataSource("/sdcard/qingyunian.mp3");
+        player.prepare();
+
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Log.d("test", "onCompletion() called with: mp = [" + mp + "]");
+            }
+        });
+        player.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Log.d("test", "onError() called with: mp = [" + mp + "], what = [" + what + "], extra = [" + extra + "]");
+                return false;
+            }
+        });
+        Looper.prepare();
+        player.addTimedTextSource("/sdcard/qingyunian.srt", MediaPlayer.MEDIA_MIMETYPE_TEXT_SUBRIP);
+
+
+        MediaPlayer.TrackInfo[] trackInfos = player.getTrackInfo();
+        Log.d("test", "MPTest: trackInfos size:" + trackInfos.length);
+
+        if (trackInfos != null && trackInfos.length > 0) {
+            for (int i = 0; i < trackInfos.length; i++) {
+                final MediaPlayer.TrackInfo info = trackInfos[i];
+                Log.w("test", "TrackInfo: " + info.getTrackType() + " "
+                        + info.getLanguage());
+
+                if (info.getTrackType() == MediaPlayer.TrackInfo.MEDIA_TRACK_TYPE_AUDIO) {
+                    // mMediaPlayer.selectTrack(i);
+                } else if (info.getTrackType() == MediaPlayer.TrackInfo.MEDIA_TRACK_TYPE_TIMEDTEXT) {
+                    player.selectTrack(i);
+                }
+            }
+        }
+        SortedMap<String,Charset> map = Charset.availableCharsets();
+
+        player.setOnTimedTextListener(new MediaPlayer.OnTimedTextListener() {
+            @Override
+            public void onTimedText(MediaPlayer mp, TimedText text) {
+                Log.d("test", "onTimedText() called with text = [" + (text != null ? text.getText() : "null") + "]");
+            }
+        });
+
+        player.start();
+
+        CountDownLatch latch = new CountDownLatch(1);
+        latch.await();
+
+        Log.d("test", "MPTest() called");
     }
 }
