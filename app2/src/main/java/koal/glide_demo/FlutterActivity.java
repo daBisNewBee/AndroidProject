283821ff1dc35872005f72b1785d286f9b2067ae
wenbin.liu <wenbin.liu@ximalaya.com>
2020-11-19 11:24:08 +0800

    注释flutter

--- app2/src/main/java/koal/glide_demo/FlutterActivity.java
+++ app2/src/main/java/koal/glide_demo/FlutterActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 
-import io.flutter.facade.Flutter;
+//import io.flutter.facade.Flutter;
 
 public class FlutterActivity extends AppCompatActivity {
 
@@ -15,8 +15,8 @@ public class FlutterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flutter);
 
-        View view = Flutter.createView(this, getLifecycle(), "/");
-        ViewGroup.LayoutParams params = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        addContentView(view, params);
+//        View view = Flutter.createView(this, getLifecycle(), "/");
+//        ViewGroup.LayoutParams params = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+//        addContentView(view, params);
     }
 }
