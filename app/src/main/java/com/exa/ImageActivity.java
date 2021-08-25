f8c032ef6283f1f3af4bd4adfd7f58be630246e5
wenbin.liu <wenbin.liu@ximalaya.com>
2021-08-26 01:10:58 +0800

    你的Bitmap占多大内存？
    比如一张 2048 * 1360 的图片占大小为：
    
    2048 * (2.75/1.5) * 1360 * (2.75/1.5)
    
    2.75：手机屏幕密度；1.5：图片所在资源文件夹密度hdpi

---
+++ app/src/main/java/com/exa/ImageActivity.java
@@ -0,0 +1,97 @@
+package com.exa;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ *
+ * bitmap 占用内存：
+ *
+ * bitmap h : 37 444 860
+ *      推导：
+ *         (440 / 240 * 2048 + 0.5) * (440 / 240 * 1360 + 0.5) * 4 = 37 444 860
+ * bitmap x : 21 063 680
+ *      推导：
+ *         (440 / 320 * 2048) * (440 / 320 * 1360) * 4 = 21 063 680
+ *
+ * "* 4"表示：每个像素4个字节
+ *      ARGB_8888格式，
+ *      整个Bitmap占用的就是宽度 * 高度 * 4 byte
+ *
+ * 图片最终的宽度和高度就是：
+ * scaledWidth = int(scaledWidth * scale + 0.5f);
+ * scaledHeight = int(scaledHeight * scale + 0.5f);
+ *
+ * 而scale为：
+ * fixme：关键！！ scale = (float) targetDensity / density（屏幕像素密度/文件夹密度）
+ * 即： 界面上显示的图片大小 = 设备dpi / 图片所在资源文件夹的最大dpi * 图片原始尺寸
+ * 440 / (320 xdpi)
+ * 或者 2.75 / 2
+ *
+ * inDensity 原始资源文件夹密度，与图片所在文件夹相关，
+ *      ldpi    ~120
+ *      mdpi    ~160      320 * 480     (基准！！1dp = 1px)
+ *      hdpi    ~240      480 * 800     1dp = 1.5px
+ *      xdpi    ~320      720 * 1080    1dp = 2px
+ *      xxdpi   ~480      1080 * 1920   1dp = 3px
+ *
+ * inTargetDensity 就是设备的屏幕密度，即：densityDpi
+ *
+ * 结论：
+ * 1. 注意图片尽量放到精度偏大的资源文件夹
+ *    同样的图片放到越小dpi的文件夹，放大倍数越大，消耗内存越大！
+ *
+ * 2. 如果手机densityDpi为 340，则匹配的是xxdpi下的图片，
+ *    因为如果匹配的是xdpi，那么图片在设备上显示时，需要放大，可能引起模糊
+ *
+ * 参考：
+ * Android中Bitmap占用内存计算：
+ * https://www.jianshu.com/p/578357ab6838
+ *
+ */
+public class ImageActivity extends AppCompatActivity {
+
+    private static final String TAG = "bitmap";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        // density=2.75, width=1080, height=2276, scaledDensity=2.75, xdpi=394.705, ydpi=394.307
+        int densityDpi = this.getResources().getDisplayMetrics().densityDpi;
+        Log.d(TAG, "densityDpi: " + densityDpi); // 440
+        Log.d(TAG, "DisplayMetrics: " + this.getResources().getDisplayMetrics());
+        // 2048 * 1360
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.shuai_h);
+        Log.d(TAG, "bitmap h getByteCount : " + bitmap.getByteCount());
+        Log.d(TAG, "bitmap h getAllocationByteCount : " + bitmap.getAllocationByteCount());
+        Log.d(TAG, "bitmap h getHeight : " + bitmap.getHeight());
+        Log.d(TAG, "bitmap h getWidth : " + bitmap.getWidth());
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.shuai_x);
+        Log.d(TAG, "bitmap x getByteCount : " + bitmap.getByteCount());
+        Log.d(TAG, "bitmap x getAllocationByteCount : " + bitmap.getByteCount());
+        Log.d(TAG, "bitmap x getHeight : " + bitmap.getHeight());
+        Log.d(TAG, "bitmap x getWidth : " + bitmap.getWidth());
+        /**
+         * bitmap h getByteCount : 37444860
+         * bitmap h getAllocationByteCount : 37444860
+         * bitmap h getHeight : 2493
+         * bitmap h getWidth : 3755
+         *
+         * bitmap x getByteCount : 21063680
+         * bitmap x getAllocationByteCount : 21063680
+         * bitmap x getHeight : 1870
+         * bitmap x getWidth : 2816
+         *
+         * 观察得出：
+         * 同样图片，相比x中加载后的，从h中加载后的图片宽、高都更大！
+         *
+         */
+
+    }
+}
